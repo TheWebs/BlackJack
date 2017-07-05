c1bdaa9c725f983bea7b22bc5cc78132cf9e5d37
Francisco <TheWebs@users.noreply.github.com>
2017-07-05 03:15:04 +0100

    Add files via upload

---
+++ BlackJack/BlackJack/Game.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlackJack
+{
+    class Game //TODO: REPLACE QUEEN, ACE, KING, JACK with proper game values (special values in blackJack)
+    {
+        private const int MAX_CARDS = 52;
+        private const int CARDS_PER_SUIT = 13; //yup, 9 cards numbered + queen + king + ace + jack xD
+        private const int NUM_SUITS = 4;
+        private int player1, player2, totalTurns, current;
+        private bool turn;
+        private Card[] deck;
+
+
+        public Game()
+        {
+            current = 0;
+            player1 = 0;
+            player2 = 0;
+            turn = true;
+            totalTurns = 0;
+            deck = createDeck();
+        }
+
+        public int getPlayer1()
+        {
+            return player1;
+        }
+
+        public int getPlayer2()
+        {
+            return player2;
+        }
+
+        public bool getTurn()
+        {
+            return turn;
+        }
+
+        public int getTotalTurns()
+        {
+            return totalTurns;
+        }
+
+        public void changeTurn()
+        {
+            turn = !turn;
+        }
+
+        public void increasePlayer(int player, int amount)
+        {
+            if (player == 1)
+                player1 += amount;
+            else
+                player2 += amount;
+        }
+
+        public void increaseTotalTurns()
+        {
+            totalTurns++;
+        }
+
+        private Card[] createDeck()
+        {
+            Card[] cards = new Card[MAX_CARDS];
+            int counter = 0;
+            for (int i = 0; i < NUM_SUITS; i++) //loop through suits
+            {
+                for (int j = 0; j < CARDS_PER_SUIT; j++)
+                {
+                    Card temp = new Card(j+1, i);
+                    cards[counter++] = temp;
+                }
+            }
+            cards = scrambleDeck(cards);
+            return cards;
+        }
+
+        private Card[] scrambleDeck(Card[] cards)
+        {
+            Random random = new Random((int)System.DateTime.Now.Ticks);
+            int rnd;
+            for (int i = 0; i < MAX_CARDS; i++)
+            {
+                rnd = random.Next(0, 52);
+                Card auxiliar = cards[i];
+                cards[i] = cards[rnd];
+                cards[rnd] = auxiliar;
+            }
+            return cards;
+        }
+
+        public void testDeck()
+        {
+            for (int i = 0; i < MAX_CARDS; i++)
+            {
+                Console.WriteLine(deck[i].ToString());
+            }
+        }
+
+        public Card turnCard() //worth it? yup cause we need to represent the suit in the main GUI
+        {
+            if (current < 52)
+                return deck[current++];
+            else
+                return null;
+        }
+
+        public string getCurrentScore(int player)
+        {
+            if( player == 1)
+                return string.Format("Player 1\n    {0}", player1);
+            else
+                return string.Format("Player 2\n    {0}", player2);
+        }
+
+        public bool checkIfBlow(int player)
+        {
+            if(player == 1)
+            {
+                if (player1 > 21)
+                {
+                    player1 = -1;
+                    return true;
+                }
+                else
+                    return false;
+            }
+            else
+            {
+                if (player2 > 21)
+                {
+                    player2 = -1;
+                    return true;
+                }
+                else
+                    return false;
+            }
+        }
+
+        public int getWinner()
+        {
+            if (player1 > player2)
+                return 1;
+            else if (player2 > player1)
+                return 2;
+            else
+                return 3;
+
+        }
+
+    }
+
+    public class Card
+    {
+        private int number;
+        private string suit;
+
+        public Card(int number, int suit)
+        {
+            this.number = number;
+            switch(suit)
+            {
+                case 0: //clubs xD
+                    this.suit = "Clubs";
+                    break;
+                case 1: //coins
+                    this.suit = "Coins";
+                    break;
+                case 2: //swords
+                    this.suit = "Swords";
+                    break;
+                case 3: //cups
+                    this.suit = "Cups";
+                    break;
+            }
+        }
+
+        public int getNumber()
+        {
+            return number;
+        }
+
+        public string getSuit()
+        {
+            return suit;
+        }
+
+        
+        override public string ToString()
+        {
+            return string.Format("{0} - {1}", this.number, this.suit);
+        }
+    }
+}
---
+++ BlackJack/BlackJack/MainWindow.xaml.cs
@@ -0,0 +1,172 @@
+﻿using BlackJack;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BlackJack
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Game game;
+        bool p1w = true, p2w = true;
+        public MainWindow()
+        {
+            InitializeComponent();
+            richTextBox.IsReadOnly = true;
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            button.Visibility = Visibility.Hidden;
+            turn_1.Visibility = Visibility.Visible;
+            stop_1.Visibility = Visibility.Visible;
+            turn_2.Visibility = Visibility.Visible;
+            stop_2.Visibility = Visibility.Visible;
+            turn_2.IsEnabled = false;
+            stop_2.IsEnabled = false;
+            play();
+        }
+
+        private void play()
+        {
+            richTextBox.Document.Blocks.Clear();
+            winner.Visibility = Visibility.Hidden;
+            button1.Visibility = Visibility.Hidden;
+            turn_1.Visibility = Visibility.Visible;
+            stop_1.Visibility = Visibility.Visible;
+            turn_2.Visibility = Visibility.Visible;
+            stop_2.Visibility = Visibility.Visible;
+            result_1.Visibility = Visibility.Visible;
+            result_2.Visibility = Visibility.Visible;
+            richTextBox.Visibility = Visibility.Visible;
+            turn_1.IsEnabled = true;
+            stop_1.IsEnabled = true;
+            turn_2.IsEnabled = false;
+            stop_2.IsEnabled = false;
+            p1w = true;
+            p2w = true;
+            game = new Game();
+            updateResult();
+        }
+
+
+        private void changeRoundButtons()
+        {
+                turn_1.IsEnabled = !turn_1.IsEnabled;
+                stop_1.IsEnabled = !stop_1.IsEnabled;
+                turn_2.IsEnabled = !turn_2.IsEnabled;
+                stop_2.IsEnabled = !stop_2.IsEnabled;
+        }
+
+        private void turn_1_Click(object sender, RoutedEventArgs e)
+        {
+            Card turnedCard = game.turnCard();
+            richTextBox.AppendText(string.Format("\nPlayer 1 turned: {0}", turnedCard.ToString()));
+            richTextBox.ScrollToEnd();
+            game.increasePlayer(1, turnedCard.getNumber());
+            if(game.checkIfBlow(1))
+            {
+                getWinner();
+            }
+            updateResult();
+            if(p2w == true)
+            {
+                changeRoundButtons();
+            }
+            
+        }
+
+        private void updateResult()
+        {
+            result_1.Content = game.getCurrentScore(1);
+            result_2.Content = game.getCurrentScore(2);
+        }
+
+        private void stop_1_Click(object sender, RoutedEventArgs e)
+        {
+            p1w = false;
+            if(p2w)
+            {
+                changeRoundButtons();
+            }
+            else
+            {
+                getWinner();
+            }
+        }
+
+        private void turn_2_Click(object sender, RoutedEventArgs e)
+        {
+            Card turnedCard = game.turnCard();
+            richTextBox.AppendText(string.Format("\nPlayer 2 turned: {0}", turnedCard.ToString()));
+            richTextBox.ScrollToEnd();
+            game.increasePlayer(2, turnedCard.getNumber());
+            if (game.checkIfBlow(2))
+            {
+                getWinner();
+            }
+            updateResult();
+            if (p1w == true)
+            {
+                changeRoundButtons();
+            }
+                
+        }
+
+        private void getWinner()
+        {
+            result_1.Visibility = Visibility.Hidden;
+            result_2.Visibility = Visibility.Hidden;
+            winner.Visibility = Visibility.Visible;
+            turn_1.Visibility = Visibility.Hidden;
+            stop_1.Visibility = Visibility.Hidden;
+            turn_2.Visibility = Visibility.Hidden;
+            stop_2.Visibility = Visibility.Hidden;
+            button1.Visibility = Visibility.Visible;
+            switch (game.getWinner())
+            {
+                case 1:
+                    winner.Content = "Player 1 is\nWinner!";
+                    break;
+                case 2:
+                    winner.Content = "Player 2 is\nWinner!";
+                    break;
+                case 3:
+                    winner.Content = "You've reached\na tie!!";
+                    break;
+            }
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            play();
+        }
+
+        private void stop_2_Click(object sender, RoutedEventArgs e)
+        {
+            p2w = false;
+            if (p1w)
+            {
+                changeRoundButtons();
+            }
+            else
+            {
+                getWinner();
+            }
+        }
+    }
+}
